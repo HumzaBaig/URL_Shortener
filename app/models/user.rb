0d0f9aa050d0403e34480aa3955e19f9af791c41
App Academy Student <student@appacademy.io>
2016-09-28 18:03:50 -0700

    Create CLI file

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
     class_name: :Visit
 
   has_many :visited_urls,
-    Proc.new { distinct }
+    Proc.new { distinct },
     through: :visits,
     source: :visited_urls
 end
